5ff319b959a87cfcfbb4aa4e0e085222fe7d0744
Sriyokeshwar S <157203076+Sriyokeshwar@users.noreply.github.com>
2026-02-08 08:28:33 +0530

    ongoing
    Will update soon

--- counter.js
+++ counter.js
@@ -12,3 +12,11 @@ function reset() {
     count = 0;
     document.getElementById("count").innerHTML = count;
 }
+window.onload = function(){
+    const h = document.getElementById("h");
+    h.classList.add("fade-out");
+    setTimeout(() => { h.innerHTML = "COUNTER APP";
+    h.classList.remove("fade-out");
+    },1000);
+    
+    };
